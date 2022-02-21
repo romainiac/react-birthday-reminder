028235b6c68dcb8e8dd3173abf4d84f532c76db1
unknown <romanyefimets@gmail.com>
2022-02-21 14:48:47 -0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <main>
+      <section className="container">
+        <h3>0 birthdays today</h3>
+      </section>
+    </main>
+  );
+};
+
+export default App;
